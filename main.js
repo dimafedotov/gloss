b80b602da7b06c472462d78f1965a7b6af9791b7
Dima Fedotov <dd.fedotov@outlook.com>
2022-06-11 13:34:10 +0300

    Transform translate

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-(function(win, doc) {
-	console.log(win);
-	console.log(doc);
+// shim anim frame
+window.requestAnimFrame = (() => {
+	return  window.requestAnimationFrame       ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
+		function(callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
 
+((win, doc) => {
 	const card = {};
 	card["container"] = doc.getElementsByClassName('parallax')[0];
 	for (let i = 0; i < card.container.children.length; i++) {
@@ -9,9 +18,7 @@
 		if (!img instanceof HTMLImageElement) continue;
 		console.log(`Found parallax image: ${img.src}`);
 		const [x, y] = img.dataset["position"]?.split(" ") || [0, 0];
-		img.style.position = "absolute";
-		img.style.left = x;
-		img.style.top = y;
+		img.style.transform = `translate3D(${x}px, ${y}px, 0)`;
 	}
 })(window, document);
 
