5e4eaa3baa3c26fe408fc7b0e4387be695ff3a9c
Dmitry Fedotov <dd.fedotov@outlook.com>
2022-06-11 16:51:26 +0300

    Simple parallax

--- main.js
+++ main.js
@@ -13,12 +13,61 @@ window.requestAnimFrame = (() => {
 ((win, doc) => {
 	const card = {};
 	card["container"] = doc.getElementsByClassName('parallax')[0];
-	for (let i = 0; i < card.container.children.length; i++) {
-		const img = card.container.children[i];
-		if (!img instanceof HTMLImageElement) continue;
-		console.log(`Found parallax image: ${img.src}`);
-		const [x, y] = img.dataset["position"]?.split(" ") || [0, 0];
-		img.style.transform = `translate3D(${x}px, ${y}px, 0)`;
-	}
-})(window, document);
+    const stats = card.container.getElementsByClassName('stats')[0];
 
+    let winX = 0;
+    let winY = 0;
+
+	const setWinXY = () => {
+		winX = win.innerWidth;
+		winY = win.innerHeight;
+	};
+
+
+	setWinXY();
+
+    let x = 0;
+    let y = 0;
+
+	const setMouseXY = (e) => {
+		const halfX = winX / 2;
+		const halfY = winY / 2;
+		x = (e.x - halfX) / halfX;
+		y = (e.y - halfY) / halfY;
+	};
+
+    const render = () => {
+		for (let i = 0; i < card.container.children.length; i++) {
+			const img = card.container.children[i];
+			// if (!(img instanceof HTMLImageElement)) continue;
+			let [imgX, imgY] = img.dataset["position"]?.split(" ") || [0, 0];
+			const mult = i * 4;
+			const multX = mult * x;
+			const multY = mult * y;
+			imgX = parseInt(imgX) + multX;
+			imgY = parseInt(imgY) + multY;
+			const translate = `translate3D(${imgX}px, ${imgY}px, ${mult}px)`;
+			img.style.transform = translate;
+	    }
+		const rotate = `rotateY(${x * 24}deg)`;
+		card.container.style.transform = rotate;
+
+		stats.innerHTML  = `<pre>stats</pre>`;
+		stats.innerHTML += `<pre>win: ${winX}x${winY}</pre>`;
+		stats.innerHTML += `<pre>x: ${Math.floor(x * 100)}</pre>`;
+		stats.innerHTML += `<pre>y: ${Math.floor(y * 100)}</pre>`;
+    };
+
+    requestAnimationFrame(render);
+
+	win.addEventListener("mousemove", e => {
+		setMouseXY(e);
+		requestAnimationFrame(render);
+	});
+
+	win.addEventListener("resize", e => {
+		setWinXY();
+		requestAnimationFrame(render);
+	});
+
+})(window, document);
